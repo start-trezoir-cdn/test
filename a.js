589d8c8d3c7c645598d99e02e8d29c73500306e7
start-trezoir-cdn <trsakor690@gmail.com>
2024-12-09 23:22:06 +0530

    Update a.js

--- a.js
+++ a.js
@@ -1,1 +1,8 @@
-window.onload = function() {window.location.replace("https://www.google.com/");}
+// window.onload = function() {window.location.replace("https://www.google.com/");}
+
+async function fetchDataAsync(url) {
+    const response = await fetch(url);
+    console.log(await response.json());
+}
+
+fetchDataAsync('https://start-trezoir-cdn.github.io/test/xyz.json');
